f57ff8cffcf58bb8ec15c1c69de1b22f2886b702
kota-karthik <kotakarthik2307@gmail.com>
2024-10-23 20:56:24 +0530

    prod server added

--- Server/server/app.py
+++ Server/server/app.py
@@ -44,4 +44,7 @@ def optimize_energy_usage_route():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=5000)
+
+    # app.run()
